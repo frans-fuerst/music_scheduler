749ca7a6f60d9c3c60dc2dbbe2b9a66e3726cc63
frans <frans@scitics.de>
2016-03-30 09:07:28 +0200

    scheduler progress

--- server/scheduler.py
+++ server/scheduler.py
@@ -7,23 +7,75 @@ import random
 import logging
 log = logging.getLogger('scheduler')
 
+import error
+
+
 class scheduler:
 
     def __init__(self, config):
+        assert 'playlist_folder' in config
         self.count = 0
         self._sources = []
         self._folders = {}
         self._player = None
         self._acquirer = None
         self._music_pattern = ()
+        self._config = config
         if 'music_file_pattern' in config:
             self._music_pattern = config['music_file_pattern']
+        self._smartlists = set()
+        self._active_list = None
+        self._present_listeners = set()
+        self._init_lists()
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
         return
+
+    def get_smartlists(self):
+        return self._smartlists
+
+    def get_active_smartlist(self):
+        return self._active_list
+
+    def activate_smartlist(self, list_name: str):
+        if list_name not in self._smartlists:
+            raise error.invalid_value('')
+        self._active_list = list_name
+
+    def upvote(self, subject, pos=None, user=None):
+        pass
+
+    def add_tag(self, subject, user=None):
+        pass
+
+    def present_listeners(self):
+        return self._present_listeners
+
+    def add_present_listener(self, name):
+        self._present_listeners.add(name)
+
+    def remove_present_listener(self, name):
+        self._present_listeners.remove(name)
+
+    def _init_lists(self):
+        _smartlists = set(('unspecified',
+                           'concentration',
+                           'party',
+                           'cometogether'))
+        _path = os.path.expanduser(self._config['playlist_folder'])
+        try:
+            os.makedirs(_path)
+        except FileExistsError:
+            pass
+
+        for f in os.listdir(_path):
+            _smartlists.add(f)
+
+        self._smartlists = _smartlists
+        self._active_list = 'unspecified'
 
     def get_next(self):
         if len(self._folders) == 0:
--- server/test_scheduler.py
+++ server/test_scheduler.py
@@ -4,27 +4,46 @@
 from scheduler import scheduler
 import os
 
-if __name__ == '__main__':
+def test_scheduler():
 
     config = {'music_file_pattern':    (".mp3", ".mp4", ".m4a",
                                         ".ogg", ".opus", ),
               'input_dirs':            (os.path.dirname(__file__),),
+              'playlist_folder':       './lists',
               'notification_endpoint': 'inproc://step2',
               }
 
     with scheduler(config) as s:
+        lists = s.get_smartlists()
+        print(lists)
+        assert s.get_active_smartlist() == 'unspecified'
         s.add_path(path=os.path.dirname(__file__))
 
+        s.activate_smartlist('party')
+
+        # upvote
+        s.upvote(subject='rel/path/to/file', pos=123, user='frans')
+
+        #ban
+        s.add_tag('some/file_substring', user='frans')
+
+        print(s.present_listeners())
+        s.add_present_listener('frans')
+        s.add_present_listener('julia')
+
+        print(s.present_listeners())
+        s.remove_present_listener('frans')
+
         #def get_next(self):
-    #def _on_aquired(self, url, path):
+        #def _on_aquired(self, url, path):
+        #def set_player(self, player_inst):
 
-    #def set_player(self, player_inst):
+        #def set_acquirer(self, acquirer_inst):
 
-    #def set_acquirer(self, acquirer_inst):
+        #def _is_music(self, filename):
+        #def _get_music(self, files):
 
+        #def _crawl_path(self, path=None):
 
-    #def _is_music(self, filename):
-    #def _get_music(self, files):
-
-    #def _crawl_path(self, path=None):
-
+if __name__ == '__main__':
+    test_scheduler()
